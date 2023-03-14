fd4d639dff36cbb205bee116126467d12dd029f3
Yurani Estrada <tyuranie@gmail.com>
2023-03-13 20:46:23 -0500

    Hice el primer ejercicio

---
+++ js-1/hello.js
@@ -0,0 +1,7 @@
+function hola (name){
+return "Hola "+ name + "!";
+}
+// código de prueba
+console.log(hola("Pedro")) // "Hola Pedro!"
+console.log(hola("Juan")) // "Hola Juan!"
+console.log(hola("")) // "Hola !"
