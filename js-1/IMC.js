1d99935d16d97d19da10680d3f457ee9062b5894
Yurani Estrada <tyuranie@gmail.com>
2023-03-13 20:59:10 -0500

    Agregando resultado ejercicio 2

---
+++ js-1/IMC.js
@@ -0,0 +1,12 @@
+//  función bmi acá
+//El BMI se calcula con la siguiente fórmula: peso / altura^2
+
+function bmi(peso, altura){
+    return peso/altura**2
+
+}
+
+// código de prueba
+console.log(bmi(65, 1.8)) // 20.061728395061728
+console.log(bmi(72, 1.6)) // 28.124999999999993
+console.log(bmi(52, 1.75)) //  16.979591836734695
